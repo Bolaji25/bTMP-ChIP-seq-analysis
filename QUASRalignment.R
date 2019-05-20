295b35a8a370a6f471941d88aabb35a3d443c0e7
Bolaji25 <45816373+Bolaji25@users.noreply.github.com>
2019-05-20 15:07:13 +0200

    Add files via upload

---
+++ QUASRalignment.R
@@ -0,0 +1,25 @@
+setwd("/")
+#to set working directory
+library(QuasR)
+library(Rsamtools)
+library(rtracklayer)
+library(GenomicFeatures)
+library(BSgenome.Celegans.UCSC.ce11)
+library(Gviz)
+sampleFile<-read.table("samples.txt")
+proj = 
+  qAlign(sampleFile="samples.txt",
+         genome= "BSgenome.Celegans.UCSC.ce11",
+         aligner="Rbowtie", 
+         projectName="data",
+         cacheDir = "/Users/imac/Desktop/Supercoilinganalysis/tocomparefastafiles/cache/")
+qQCReport(proj, pdfFilename="tocompare2.pdf")
+
+qExportWig(proj, scaling=F)
+qExportWig(proj, binsize=100L, scaling=TRUE, collapseBySample=TRUE)
+#to align using QuasR
+#to create pdf of the q
+
+#to export genome wig file from alignments
+library(GenomicRanges)
+#to load genomic ranges library
