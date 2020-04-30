56fc71ae7dc66f5e7641b0d5a54d67cbbcfc411c
Isiaka Bolaji Nafisat (bi18k694) <bi18k694@submit02.ubelix.unibe.ch>
2020-04-30 18:26:12 +0200

    mming

--- trimmed.sh
+++ trimmed.sh
@@ -2,7 +2,7 @@
 
 ## Allocate resources
 #SBATCH --time=1-12:00:00
-#SBATCH --array=1-4
+#SBATCH --array=1
 
 #SBATCH --mail-user=bolaji.isiaka@izb.unibe.ch
 #SBATCH --mail-type=end,fail
@@ -15,7 +15,7 @@
 module add vital-it;
 module add UHTS/Quality_control/cutadapt/2.5;
 
-module load R/3.6.1;
+module load R/3.5.1;
 module add UHTS/Quality_control/fastqc/0.11.7;      
 module add UHTS/Analysis/trimmomatic/0.36;
 module add UHTS/Analysis/samtools/1.8;
@@ -29,16 +29,20 @@ module add UHTS/Analysis/bamtools/2.4.1;
 # Install Trim Galore
 curl -fsSL https://github.com/FelixKrueger/TrimGalore/archive/0.6.5.tar.gz -o trim_galore.tar.gz
 tar xvzf trim_galore.tar.gz
+#PATH=$PATH: /home/ubelix/izb/bi18k694/PhD_analysis/supercoiling/analysis/scripts/TrimGalore-0.6.5
+#export PATH
 # Run Trim Galore
-./TrimGalore-0.6.5/trim_galore
+#./TrimGalore-0.6.5/trim_galore
 
-mkdir ./trimmed/
+#mkdir ./trimmedreseq/
+#mkdir ./qcreseq/
 
-#gunzip ../rawData/masters/*.fastq.gz 
-fastqc /home/ubelix/izb/bi18k694/PhD_analysis/supercoiling/analysis/rawData/masters/*.fastq -o ./qc/
-trim_galore -q 20 /home/ubelix/izb/bi18k694/PhD_analysis/supercoiling/analysis/rawData/masters/*.fastq -o ./trimmed/
-fastqc ./trimmed/*.fq -o ./qc/
+#gunzip ../rawData/reseq2020110/*.fastq.gz
+#fastqc /home/ubelix/izb/bi18k694/PhD_analysis/supercoiling/analysis/rawData/reseq2020110/*.fastq -o ./qcreseq/
+#the clip is to clip off 50bp from the 100bp reads to make it similar to the previous, omit this option for 50bp read length reads
+./TrimGalore-0.6.5/trim_galore -q 20 --three_prime_clip_R1 50  /home/ubelix/izb/bi18k694/PhD_analysis/supercoiling/analysis/rawData/reseq2020110/*.fastq -o ./trimmedreseq/
+fastqc ./trimmedreseq/*.fq -o ./qcreseq/
 
-# get index for list of samples
-#let i=$SLURM_ARRAY_TASK_ID-1
+
+
 
