8eb8a172fbefcf3cd34aa17e82bb300fa6b083a2
Isiaka Bolaji Nafisat (bi18k694) <bi18k694@submit01.ubelix.unibe.ch>
2020-05-08 15:19:56 +0200

    okay

--- chipalignment.sh
+++ chipalignment.sh
@@ -27,7 +27,7 @@ module add UHTS/Analysis/sambamba/0.7.1;
 
 # initialize a variable with an intuitive name to store the name of the input fastq file
 base=$1
-fq=(`ls /home/ubelix/izb/bi18k694/PhD_analysis/supercoiling/analysis/scripts/reseq/trimmedreseq/${base}*.fq`) 
+fq=(`ls /home/ubelix/izb/bi18k694/PhD_analysis/supercoiling/analysis/bTMPseq/trimmed2020110/${base}*.fq`) 
 
 # grab base of filename for naming outputs
 base=`basename $fq .fq`
@@ -39,45 +39,48 @@ genome=./reference/c.elegans_270
 # make all of the output directories
 # The -p option means mkdir will create the whole path if it 
 # does not exist and refrain from complaining if it does exist
-mkdir -p ./reseq/fastqc
-mkdir -p ./reseq/intermediate_bams
-
-# set up output filenames and locations
-fastqc_out=./reseq/fastqc/
-
-## set up file names
-align_out=./reseq/${base}_unsorted.sam
-align_bam=./reseq/${base}_unsorted.bam
-align_sorted=./reseq/${base}_sorted.bam
-align_filtered=./reseq/${base}_aln.bam
-align_flag=./reseq/${base}_aln.txt
-
-## set up more variables for 2 additional directoties to help clean up the results folder
-bowtie_results=./reseq
-intermediate_bams=./reseq/intermediate_bams
-
+#mkdir -p ./reseq/fastqc
+#mkdir -p ./reseq/intermediate_bams
+#
+## set up output filenames and locations
+#fastqc_out=./reseq/fastqc/
+#
+### set up file names
+align_out=./trimmed2020110/sam/${base}_unsorted.sam
+align_bam=./trimmed2020110/bam/${base}_unsorted.bam
+align_sorted=./trimmed2020110/bam/${base}_sorted.bam
+align_filtered=./trimmed2020110/bam/${base}_aln.bam
+align_flag=./trimmed2020110/${base}_aln.txt
+align_flag2=./trimmed2020110/bam/text/${base}_aln2.txt
+#
+### set up more variables for 2 additional directoties to help clean up the results folder
+#bowtie_results=./reseq
+#intermediate_bams=./reseq/intermediate_bams
+#
 echo "Processing file $fq"
-
-# Run FastQC and move output to the appropriate folder
-fastqc $fq
-
-# Run bowtie2
+#
+## Run FastQC and move output to the appropriate folder
+#fastqc $fq
+#
+## Run bowtie2
 bowtie2 -p 8 -q --local -x $genome -U $fq -S $align_out
-
-# Create BAM from SAM
+#
+## Create BAM from SAM
 samtools view -h -S -b -@ 8 -o $align_bam $align_out
-
-#check stat 
+#
+##check stat 
 samtools flagstat  $align_bam > $align_flag
+#
+## Sort BAM file by genomic coordinates
+samtools sort -@ 8 -o $align_sorted $align_bam
 
-# Sort BAM file by genomic coordinates
-#samtools sort -@ 8 -o $align_sorted $align_bam
-
-# Filter out duplicates
-#sambamba view -h -t 8 -f bam -F "[XS] == null and not unmapped " $align_bam > $align_filtered
-
-# Create indices for all the bam files for visualization and QC
-#samtools index $align_filtered
-
-# Move intermediate files out of the bowtie2 directory
+samtools flagstat $align_sorted > $align_flag2
+#
+## Filter out duplicates
+##sambamba view -h -t 8 -f bam -F "[XS] == null and not unmapped " $align_bam > $align_filtered
+#
+## Create indices for all the bam files for visualization and QC
+##samtools index $align_filtered
+#
+## Move intermediate files out of the bowtie2 directory
 #mv $bowtie_results/${base}*sorted* $intermediate_bams
--- removedup.sh
+++ removedup.sh
@@ -2,7 +2,7 @@
 
 ## Allocate resources
 #SBATCH --time=1-24:00:00
-#SBATCH --array=1
+#SBATCH --array=1-7
 
 #SBATCH --mail-user=bolaji.isiaka@izb.unibe.ch
 #SBATCH --mail-type=end,fail
@@ -14,36 +14,45 @@
 
 #module add vital-it;
 #module add Development/java/11.0.6;
-#module add UHTS/Analysis/samtools/1.10;
+module add UHTS/Analysis/samtools/1.10;
 
 base=$1
-fq=(`ls /home/ubelix/izb/bi18k694/PhD_analysis/supercoiling/analysis/bTMPseq/reseq/${base}*.bam`)
+fq=(`ls /home/ubelix/izb/bi18k694/PhD_analysis/supercoiling/analysis/bTMPseq/trimmed2020110/bam/${base}*.bam`)
 base=`basename $fq .bam`
 echo "Sample name is $base"
 
-align_sorted=./reseq/${base}_sorted.bam
-align_duprem=./reseq/${base}_duprem.bam
-align_qc=./reseq/${base}_Duprem.txt
-align_flag=./reseq/${base}_dupflag.txt
+align_sorted=./trimmed2020110/bam/${base}_sorted.bam
+align_duprem=./trimmed2020110/bam/${base}.bam
+align_qc=./trimmed2020110/bam/${base}_Duprem.txt
+align_flag=./trimmed2020110/bam/${base}_dupflag.txt
+align_sortge=./trimmed2020110/bam/${base}_cordsort.bam
+align_flag2=./trimmed2020110/bam/${base}_dupflag2.txt
 
-mkdir -p ./reseq/intermediate_dups
+#mkdir -p ./reseq/intermediate_dups
 intermediate_dups=./reseq/intermediate_dups
 
 #${picardDIR} = /home/ubelix/izb/bi18k694/PhD_analysis/supercoiling/analysis/bTMPseq/
 
 #sort by query name for duplicate removal
 java -Xms1g -Xmx8g -jar /home/ubelix/izb/bi18k694/PhD_analysis/supercoiling/analysis/bTMPseq/picard.jar SortSam I=$fq O=$align_sorted SORT_ORDER=queryname TMP_DIR=$intermediate_dups
-
-
-# remove duplicates with picard (path to picard should be set in $PICARD variable in .bash_profile or in session)
-# Note that to mark unmapped mates of mapped records and supplementary/secondary alignments as duplicates the bam
-# file must be querysorted (by name) not by coordinate. 
+#
+#
+## remove duplicates with picard (path to picard should be set in $PICARD variable in .bash_profile or in session)
+## Note that to mark unmapped mates of mapped records and supplementary/secondary alignments as duplicates the bam
+## file must be querysorted (by name) not by coordinate. 
 java -Xms1g -Xmx8g -jar /home/ubelix/izb/bi18k694/PhD_analysis/supercoiling/analysis/bTMPseq/picard.jar MarkDuplicates I=$align_sorted O=$align_duprem M=$align_qc REMOVE_DUPLICATES=true REMOVE_SEQUENCING_DUPLICATES=true  
 ASSUME_SORT_ORDER=queryname TMP_DIR=$intermediate_dups
-
-
-
+#
+#
+#
 rm $align_sorted
+#
+##get stats
 
-#get stats
 samtools flagstat  $align_duprem > $align_flag
+
+samtools sort -o $align_sortge $align_duprem
+samtools flagstat  $align_sortge > $align_flag2
+
+samtools index $align_sortge
+
